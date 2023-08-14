fd0d37f50e62605c1fcb81a27dacf6260e875504
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-14 13:41:44 +0700

    feat: make route for get thread by id, like thread, get thread likes, and unlike the thread

--- src/routes/threads.js
+++ src/routes/threads.js
@@ -5,6 +5,12 @@ const threadsController = require('../controllers/threads');
 
 router.get('/threads', threadsController.getThreads);
 router.post('/threads', threadsController.createThread);
+
+router.get('/threads/:id', threadsController.getThreadById);
 router.delete('/threads/:id', threadsController.deleteThread);
+
+router.get('/threads/:id/likes', threadsController.getLikesThread);
+router.post('/threads/:id/likes', threadsController.likeThread);
+router.delete('/threads/:id/likes', threadsController.unlikeThread);
 
 module.exports = router;
