93a3be57fac312cfa0119d9394ff86407778c64f
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-14 13:46:01 +0700

    fix: get all type notifications

--- src/controllers/notifications.js
+++ src/controllers/notifications.js
@@ -1,5 +1,6 @@
 const connectToDatabase = require('../database/database');
 const User = require('../database/models/user');
+const Thread = require('../database/models/thread');
 const Notifications = require('../database/models/notifications');
 
 const getAllNotifications = async (req, res) => {
@@ -10,11 +11,50 @@ const getAllNotifications = async (req, res) => {
   try {
     const notifications = await Notifications.find({
       user_id: user_id,
-    }).populate('data', ['username', 'image']);
+    });
+
+    const notificationsByType = {
+      follow: [],
+      like: [],
+      comment: [],
+    };
+
+    notifications.forEach((notification) => {
+      notificationsByType[notification.type].push(notification);
+    });
+
+    for (const type in notificationsByType) {
+      const dataModel = type === 'like' ? 'Thread' : 'User'; // Ganti dengan model yang sesuai
+      if (dataModel) {
+        await Notifications.populate(notificationsByType[type], {
+          path: 'data',
+          model: dataModel,
+        });
+
+        // Populate creator untuk semua jenis notifikasi
+        await Notifications.populate(notificationsByType[type], {
+          path: 'data.creator',
+          model: 'User',
+        });
+
+        if (type === 'like') {
+          await Notifications.populate(notificationsByType[type], {
+            path: 'actionUser',
+            model: 'User',
+          });
+        }
+      }
+    }
+
+    const formattedNotifications = {
+      follow: notificationsByType.follow,
+      like: notificationsByType.like,
+      comment: notificationsByType.comment,
+    };
 
     return res.json({
       message: 'Success Get All Notifications',
-      data: notifications,
+      data: formattedNotifications,
     });
   } catch (error) {
     console.log(error);
